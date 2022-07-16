076bae70391abd4cc625e5caf31729c13fecc651
Никита Бочаров <bochanikita@yandex.ru>
2022-07-16 14:02:19 +0300

    Add "Something else"

--- BochaTestGit1/Program.cs
+++ BochaTestGit1/Program.cs
@@ -9,6 +9,7 @@ namespace BochaTestGit1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Test Git");
             Console.WriteLine("New string");
+            Console.WriteLine("Something else");
         }
     }
 }
